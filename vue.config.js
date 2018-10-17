0402879697981bcc1956f4140bd0fb6700d29932
Kasonsx <sxk644653293@163.com>
2018-10-17 22:56:09 +0800

    updated vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
     baseUrl: process.env.NODE_ENV === 'production'
         ? '/calculator/'
-        : '/'
+        : '/',
+    indexPath: '../index.html',
+    filenameHashing: false,
 }
